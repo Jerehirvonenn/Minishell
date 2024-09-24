d05b50fd70d395bca3af237c532bf7ef6aefc122
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2024-09-24 16:15:42 +0300

    changes in main.c to check how execute commands

--- main.c
+++ main.c
@@ -6,17 +6,37 @@ bool	special_buildin(int ac, char **av);
 int	main(int ac, char **av)
 {
 	t_shell ms;
+	t_token *tokens;
+	t_ast	*ast;
 	char	*input;
+	int status = 0;
 
 	init_shell(&ms);
 	//this is for cases of special buildins
 	/*if (special_buildin(ac, av))
 		execute_buildin(&ms, av);*/
-	execute_buildin(&ms, av);
-	/*while (true)
+	while (true)
 	{
 		input = readline("\033[33mminishell\033[0m\033[35m$\033[0m ");
-		if (input)
+		if (*input)
             		add_history(input); // Add input to history
-        }*/
+		tokens = tokenize(input); // Implement your own tokenizer
+		if (!tokens)
+		{
+			free(input);
+			continue; // In case of tokenization failure
+		}
+		ast = parsing_ast(tokens);
+		if (!ast)
+		{
+			free_tokens(tokens);
+			free(input);
+			continue; // In case of AST parsing failure
+		}
+		status = execute_ast(ast);
+		// Cleanup
+		free_ast(ast);
+		free_tokens(tokens);
+		free(input);
+        }
 }
