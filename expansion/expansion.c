72f9f261e03c7f7bfe9dc6c840e6f1742a8f3f9d
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2024-10-24 15:21:56 +0300

    changes in expansion.c

--- expansion/expansion.c
+++ expansion/expansion.c
@@ -1,17 +1,17 @@
 #include "../includes/minishell.h"
 
 // Updated handle_cases function to manage quotes
-void	handle_cases(char **clean, char *ins, size_t *i, t_ms *ms)
+static void	handle_cases(char **clean, char *ins, size_t *i, t_ms *ms)
 {
 	if (ft_isquote(ins[*i]))
 	{
-		//fprintf(stderr, "quotation\n");
+	//	fprintf(stderr, "quotation\n");
 		handle_quoted_literal(clean, ins, i, ms);
 	}
 	else if (ins[*i] == '$' && ft_isdelim(ins[*i + 1]))
 	{
-		//fprintf(stderr, "delimetr\n");
-		handle_normal_char(clean, ins, i);
+	//	fprintf(stderr, "delimetr\n");
+		handle_normal_char(clean, ins, i, ms);
 	}
 	else if (ins[*i] == '$' && ins[*i + 1] == '?')
 	{
@@ -20,11 +20,11 @@ void	handle_cases(char **clean, char *ins, size_t *i, t_ms *ms)
 	}
 	else if (ins[*i] == '$' && (ft_isalnum(ins[*i + 1]) || ins[*i + 1] == '_'))
 	{
-		//fprintf(stderr, "env var\n");
+	//	fprintf(stderr, "env var\n");
 		handle_envir(clean, ins, i, ms);
 	}
 	else
-		handle_normal_char(clean, ins, i);
+		handle_normal_char(clean, ins, i, ms);
 }
 
 // Expand argument with variable and quote handling
@@ -34,40 +34,60 @@ char	*expand_argument(char *arg, t_ms *ms)
 	size_t	j;
 
 	expanded_arg = ft_strdup("");
-	j = 0;
 	if (!expanded_arg)
-		return (NULL);
-	while (arg[j])
 	{
-		//printf("Handling character: %c (at index %zu)\n", arg[j], j);
-		handle_cases(&expanded_arg, arg, &j, ms);
+		ft_free_ast(ms->ast);
+		clean_ms(ms);
+		printf("minishell: cannot allocate memory\n");
+		exit(1);
 	}
+	j = 0;
+	while (arg[j])
+		handle_cases(&expanded_arg, arg, &j, ms);
 	return (expanded_arg);
 }
 
-// Expand arguments in the AST
 void	expand_ast(t_ast *node, t_ms *ms)
 {
 	size_t	i;
-	char	*expanded_arg;
+	char	*new_value;
+	t_io	*temp_io;
 
-	i = 0;
-	if (!node || !node->exp_value)
+	if (!node)
 		return ;
-	while (node->exp_value[i] != NULL)
+	i = 0;
+	new_value = NULL;
+	while (node->exp_value && node->exp_value[i])
 	{
-		if (ft_strchr(node->exp_value[i], '$')
-			|| ft_strchr(node->exp_value[i], '\"')
-			|| ft_strchr(node->exp_value[i], '\''))
+		new_value = expand_argument(node->exp_value[i], ms);
+		if (!new_value)
 		{
-			//printf("Expan arg: %s, from index 0\n", node->exp_value[i]);
-			expanded_arg = expand_argument(node->exp_value[i], ms);
-			if (expanded_arg)
-			{
-				free(node->exp_value[i]);
-				node->exp_value[i] = expanded_arg;
-			}
+			ft_free_ast(ms->ast);
+			clean_ms(ms);
+			printf("minishell: cannot allocate memory\n");
+			exit(1);
 		}
+		free(node->exp_value[i]);
+		node->exp_value[i] = new_value;
 		i++;
+		temp_io = node->io_list;
+		while (temp_io && temp_io->value && temp_io->type != T_HEREDOC)
+		{
+			new_value = expand_argument(temp_io->value, ms);
+			if (!new_value)
+			{
+				ft_free_ast(ms->ast);
+				clean_ms(ms);
+				printf("minishell: cannot allocate memory\n");
+				exit(1);
+			}
+			free(temp_io->value);
+			temp_io->value = new_value;
+			temp_io = temp_io->next;
+		}
 	}
+	if (node->left)
+		expand_ast(node->left, ms);
+	if (node->right)
+		expand_ast(node->right, ms);
 }
